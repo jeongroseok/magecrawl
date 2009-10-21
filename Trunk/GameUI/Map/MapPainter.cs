b3ec46d9b6f2d09cbe040897fe990e64a3f07580
chris hamons <chris.hamons@gmail.com>
2009-10-20 19:42:18 -0500

    Refactor GameUI map painters to seperate classes that pipeline to build up the map.

---
+++ Trunk/GameUI/Map/MapPainter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using libtcodWrapper;
+using Magecrawl.GameEngine.Interfaces;
+using Magecrawl.Utilities;
+
+namespace Magecrawl.GameUI.Map
+{
+    public sealed class MapPainter : MapPainterBase
+    {
+        private Console m_offscreenConsole;
+
+        public MapPainter()
+        {
+            m_offscreenConsole = RootConsole.GetNewConsole(OffscreenWidth, OffscreenHeight);
+        }
+
+        public override void UpdateFromNewData(IGameEngine engine)
+        {
+            m_offscreenConsole.Clear();
+
+            m_offscreenConsole.DrawFrame(0, 0, MapDrawnWidth + 1, MapDrawnHeight + 1, true, "Map");
+
+            Point mapUpCorner = CalculateMapCorner(engine.Player);
+
+            for (int i = 0; i < engine.Map.Width; ++i)
+            {
+                for (int j = 0; j < engine.Map.Height; ++j)
+                {
+                    DrawThing(mapUpCorner, new Point(i, j), m_offscreenConsole, ConvertTerrianToChar(engine.Map[i, j].Terrain));
+                }
+            }
+
+            foreach (IMapObject obj in engine.Map.MapObjects)
+            {
+                DrawThing(mapUpCorner, obj.Position, m_offscreenConsole, ConvertMapObjectToChar(obj.Type));
+            }
+
+            foreach (ICharacter obj in engine.Map.Monsters)
+            {
+                DrawThing(mapUpCorner, obj.Position, m_offscreenConsole, 'M');
+            }
+
+            m_offscreenConsole.PutChar(ScreenCenter.X + 1, ScreenCenter.Y + 1, '@');
+        }
+
+        public override void DrawNewFrame(Console screen)
+        {
+            m_offscreenConsole.Blit(0, 0, OffscreenWidth, OffscreenHeight, screen, 0, 0);
+        }
+
+        public override void Dispose()
+        {
+            if (m_offscreenConsole != null)
+                m_offscreenConsole.Dispose();
+            m_offscreenConsole = null;
+        }
+
+        public override void HandleRequest(string request, object data)
+        {
+        }
+
+        private static void DrawThing(Point mapUpCorner, Point position, Console screen, char symbol)
+        {
+            Point screenPlacement = new Point(mapUpCorner.X + position.X + 1, mapUpCorner.Y + position.Y + 1);
+
+            if (IsDrawableTile(screenPlacement))
+            {
+                screen.PutChar(screenPlacement.X, screenPlacement.Y, symbol);
+            }
+        }
+    }
+}
---
+++ Trunk/GameUI/Map/MapPaintingCoordinator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using libtcodWrapper;
+using Magecrawl.GameEngine.Interfaces;
+
+namespace Magecrawl.GameUI.Map
+{
+    public sealed class MapPaintingCoordinator : System.IDisposable
+    {
+        List<MapPainterBase> m_painters;
+        
+        public MapPaintingCoordinator()
+        {
+            m_painters = new List<MapPainterBase>();
+
+            // The map painter is special since it should go first
+            m_painters.Add(new MapPainter());
+            m_painters.Add(new MapDebugMovablePainter());
+            m_painters.Add(new MapEffectsPainter());
+        }
+
+        public void UpdateFromNewData(IGameEngine engine)
+        {
+            foreach (MapPainterBase p in m_painters)
+            {
+                p.UpdateFromNewData(engine);
+            }
+        }
+
+        public void DrawNewFrame(Console console)
+        {
+            foreach (MapPainterBase p in m_painters)
+            {
+                p.DrawNewFrame(console);
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (MapPainterBase p in m_painters)
+            {
+                p.Dispose();
+            }
+            m_painters = null;
+        }
+
+        public void HandleRequest(string request, object data)
+        {
+            foreach (MapPainterBase p in m_painters)
+            {
+                p.HandleRequest(request, data);
+            }
+        }
+    }
+}
