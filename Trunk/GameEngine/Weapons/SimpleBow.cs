7b3dc011e48e8fd7b51d0515d31d3bb48e34e0e1
chris hamons <chris.hamons@gmail.com>
2009-10-24 10:39:05 -0500

    Submit initial work on IWeapon so I can merge in cleanup/instance of CoreGameEngine.

---
+++ Trunk/GameEngine/Weapons/SimpleBow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Magecrawl.GameEngine.Interfaces;
+using Magecrawl.Utilities;
+
+namespace Magecrawl.GameEngine.Weapons
+{
+    internal class SimpleBow : IWeapon
+    {   
+        internal SimpleBow()
+        {
+        }
+
+        public DiceRoll Damage
+        {
+            get 
+            {
+                return new DiceRoll(1, 1);
+            }
+        }
+
+        public string Name
+        {
+            get 
+            {
+                return "Bow";
+            }
+        }
+
+        public List<Point> TargetablePoints(Point characterPosition)
+        {
+            List<Point> targetablePoints = new List<Point>();
+
+            const int SimpleBowRange = 4;
+            for (int i = -SimpleBowRange; i <= SimpleBowRange; ++i)
+            {
+                for (int j = -SimpleBowRange; j <= SimpleBowRange; ++j)
+                {
+                    bool allowable = (System.Math.Abs(i) + System.Math.Abs(j)) <= SimpleBowRange;
+                    if (allowable)
+                        targetablePoints.Add(new Point(characterPosition.X + i, characterPosition.Y + j));
+                }
+            }
+            
+            return targetablePoints;
+        }
+    }
+}
--- Trunk/GameUI/Map/PlayerAttackRangePainter.cs
+++ Trunk/GameUI/Map/PlayerAttackRangePainter.cs
@@ -7,7 +7,7 @@ namespace Magecrawl.GameUI.Map
     internal sealed class PlayerAttackRangePainter : MapPainterBase
     {
         private Point m_playerPosition;
-        private int m_range;
+        private IWeapon m_equipedWeapon;
         private bool m_enabled;
         private Point m_mapUpCorner;
         private int m_mapWidth;
@@ -16,7 +16,7 @@ namespace Magecrawl.GameUI.Map
         public PlayerAttackRangePainter()
         {
             m_playerPosition = new Point();
-            m_range = 0;
+            m_equipedWeapon = null;
             m_enabled = false;
             m_mapUpCorner = new Point();
             m_mapHeight = 0;
@@ -27,7 +27,7 @@ namespace Magecrawl.GameUI.Map
         {
             m_mapUpCorner = mapUpCorner;
             m_playerPosition = engine.Player.Position;
-            m_range = engine.Player.RangedAttackDistance;
+            m_equipedWeapon = engine.Player.CurrentWeapon;
             m_mapHeight = engine.Map.Height;
             m_mapWidth = engine.Map.Width;
         }
@@ -36,17 +36,15 @@ namespace Magecrawl.GameUI.Map
         {
             if (m_enabled)
             {
-                for (int i = 0; i < m_mapWidth; ++i)
+                foreach (Point position in m_equipedWeapon.TargetablePoints(m_playerPosition))
                 {
-                    for (int j = 0; j < m_mapHeight; ++j)
+                    Point screenPlacement = new Point(m_mapUpCorner.X + position.X + 1, m_mapUpCorner.Y + position.Y + 1);
+
+                    if (IsDrawableTile(screenPlacement))
                     {
-                        bool allowable = System.Math.Abs(i - m_playerPosition.X) + System.Math.Abs(j - m_playerPosition.Y) <= m_range;
-                        if (allowable)
-                        {
-                            Color currentColor = screen.GetCharBackground(m_mapUpCorner.X + i + 1, m_mapUpCorner.Y + j + 1);
-                            Color newColor = Color.Interpolate(currentColor, TCODColorPresets.DarkYellow, .5f);
-                            screen.SetCharBackground(m_mapUpCorner.X + i + 1, m_mapUpCorner.Y + j + 1, newColor);
-                        }
+                        Color currentColor = screen.GetCharBackground(screenPlacement.X, screenPlacement.Y);
+                        Color newColor = Color.Interpolate(currentColor, TCODColorPresets.DarkYellow, .5f);
+                        screen.SetCharBackground(screenPlacement.X, screenPlacement.Y, newColor);
                     }
                 }
             }
