42cbdb234fae2455d00dd627df1de3137fdb029e
chris hamons <chris.hamons@gmail.com>
2009-10-25 23:50:28 -0500

    Remove duplication in weapons code.

--- Trunk/GameEngine/Weapons/Spear.cs
+++ Trunk/GameEngine/Weapons/Spear.cs
@@ -11,9 +11,6 @@ namespace Magecrawl.GameEngine.Weapons
     // This class should go away when we have an xml reader to create items on the fly. Until then...
     internal class Spear : WeaponBase, Item
     {
-        private string m_itemDescription;
-        private string m_flavorText;
-
         internal Spear(string name, DiceRoll damage, string description, string flavorText)
         {
             m_itemDescription = description;
@@ -21,48 +18,6 @@ namespace Magecrawl.GameEngine.Weapons
             m_owner = null;
             m_name = name;
             m_damage = damage;
-        }
-
-        public string ItemDescription
-        {
-            get 
-            {
-                return m_itemDescription;
-            }
-        }
-
-        public string FlavorDescription
-        {
-            get
-            {
-                return m_flavorText;
-            }
-        }
-
-        public List<ItemOptions> PlayerOptions
-        {
-            get
-            {
-                return new List<ItemOptions>() 
-                {
-                    new ItemOptions("Equip", true),
-                    new ItemOptions("Drop", true)
-                };
-            }
-        }
-
-        public XmlSchema GetSchema()
-        {
-            return null;
-        }
-
-        public void ReadXml(XmlReader reader)
-        {
-        }
-
-        public void WriteXml(XmlWriter writer)
-        {
-            writer.WriteElementString("Type", m_name);
         }
 
         public override List<WeaponPoint> CalculateTargetablePoints()
