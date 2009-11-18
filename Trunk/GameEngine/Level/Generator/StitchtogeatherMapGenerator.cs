e3b750ead798c717689b4c2b15470264e8bc9514
chris hamons <chris.hamons@gmail.com>
2009-11-17 23:23:38 -0600

    Pull lots of stuff out of MapGenerator into other classes.

--- Trunk/GameEngine/Level/Generator/StitchtogeatherMapGenerator.cs
+++ Trunk/GameEngine/Level/Generator/StitchtogeatherMapGenerator.cs
@@ -20,7 +20,7 @@ namespace Magecrawl.GameEngine.Level.Generator
         private List<MapChunk> m_treasureRooms;
         private List<MapChunk> m_sideRooms;
         private Point m_playerPosition;
-        private StitchRatio m_stitchRatio;
+        private StitchtogeatherMapGraphGenerator m_graphGenerator;
 
         internal StitchtogeatherMapGenerator()
         {
@@ -31,73 +31,13 @@ namespace Magecrawl.GameEngine.Level.Generator
             m_treasureRooms = new List<MapChunk>();
             m_sideRooms = new List<MapChunk>();
             m_playerPosition = Point.Invalid;
-            m_stitchRatio = new StitchRatio(100);
+            m_graphGenerator = new StitchtogeatherMapGraphGenerator();
 
             LoadChunksFromFile("Map" + Path.DirectorySeparatorChar + "DungeonChunks.dat");
         }
 
-        private struct ParenthoodElement
-        {
-            public MapChunk Chunk;
-            public Point UpperLeft;
-            public Point Seam;
-
-            public ParenthoodElement(MapChunk chunk, Point upperLeft, Point seam)
-            {
-                Chunk = chunk;
-                UpperLeft = upperLeft;
-                Seam = seam;
-            }
-        }
-
-        private class ParenthoodChain
-        {
-            public Stack<MapChunk> Chunks { get; set; }
-            public Stack<Point> UpperLefts { get; set; }
-            public Stack<Point> Seams { get; set; }
-
-            public ParenthoodChain()
-            {
-                Chunks = new Stack<MapChunk>();
-                UpperLefts = new Stack<Point>();
-                Seams = new Stack<Point>();
-            }
-
-            public ParenthoodChain(ParenthoodChain p)
-            {
-                // There is no Stack constructor that take a stack, just the enumerable one
-                // Since we want a shallow copy, we'll just reverse the list and use that
-                Chunks = new Stack<MapChunk>(p.Chunks.Reverse());
-                UpperLefts = new Stack<Point>(p.UpperLefts.Reverse());
-                Seams = new Stack<Point>(p.Seams.Reverse());
-            }
-
-            public void Push(MapChunk c, Point upperLeft, Point seam)
-            {
-                Chunks.Push(c);
-                UpperLefts.Push(upperLeft);
-                Seams.Push(seam);
-            }
-
-            public ParenthoodElement Pop()
-            {
-                ParenthoodElement top = new ParenthoodElement(Chunks.Peek(), UpperLefts.Peek(), Seams.Peek());
-                Chunks.Pop();
-                UpperLefts.Pop();
-                Seams.Pop();
-                return top;
-            }
-
-            public ParenthoodElement Peek()
-            {
-                return new ParenthoodElement(Chunks.Peek(), UpperLefts.Peek(), Seams.Peek());
-            }
-        }
-
         internal Map GenerateMap(out Point playerPosition)
         {
-            MapNode graphHead = GenerateMapGraph();
-
             int width = 250;
             int height = 250;
             Map map = new Map(width, height);
@@ -105,8 +45,10 @@ namespace Magecrawl.GameEngine.Level.Generator
             // Point center = new Point(m_random.GetRandomInt(100, 150), m_random.GetRandomInt(100, 150));
             Point center = new Point(100, 100);
 
+            MapNode graphHead = m_graphGenerator.GenerateMapGraph();
+
             ParenthoodChain parentChain = new ParenthoodChain();
-            
+   
             GenerateMapFromGraph(graphHead, map, center, parentChain);
 
             playerPosition = m_playerPosition;
@@ -120,13 +62,6 @@ namespace Magecrawl.GameEngine.Level.Generator
             return map;
         }
 
-        private void PrintDebugString(string s)
-        {
-            #if DEBUG
-                System.Console.Out.WriteLine(s);
-            #endif
-        }
-
         private MapChunk GetRandomChunkFromList(List<MapChunk> chunk)
         {
             return new MapChunk(chunk[m_random.GetRandomInt(0, chunk.Count - 1)]);
@@ -136,8 +71,6 @@ namespace Magecrawl.GameEngine.Level.Generator
         {
             if (current.Generated)
                 return;
-
-            PrintDebugString(string.Format("Generating {0} - {1}", current.Type.ToString(), current.UniqueID.ToString()));
 
             current.Generated = true;
             bool placed = false;
@@ -258,132 +191,6 @@ namespace Magecrawl.GameEngine.Level.Generator
                 currentChunk.Seams.Remove(seam);
                 GenerateMapFromGraph(nextNode, map, seam + upperLeft, parentChain);
             }
-        }
-
-        private int NumberOfNeighborsToGenerate(MapNodeType current)
-        {
-            switch (current)
-            {
-                case MapNodeType.Entrance:
-                    return 4;
-                case MapNodeType.Hall:
-                    return 1;
-                case MapNodeType.MainRoom:
-                case MapNodeType.SideRoom:
-                case MapNodeType.TreasureRoom:
-                    return 3;
-                case MapNodeType.None:
-                    return 0;
-                case MapNodeType.NoneGivenYet:
-                default:
-                    throw new ArgumentException("NumberOfNeighborsToGenerate - No valid number of neighbors");
-            }
-        }
-
-        private MapNode GenerateMapGraph()
-        {
-            MapNode graphHead = new MapNode(MapNodeType.Entrance);
-
-            Queue<MapNode> nodesToHandle = new Queue<MapNode>();
-            nodesToHandle.Enqueue(graphHead);
-
-            while (nodesToHandle.Count > 0)
-            {
-                MapNode currentNode = nodesToHandle.Dequeue();
-
-                for (int i = 0; i < NumberOfNeighborsToGenerate(currentNode.Type); ++i)
-                {
-                    MapNodeType nodeTypeToGenerate = m_stitchRatio.GetNewNode(currentNode.Type);
-                    AddNeighborsToNode(currentNode, nodeTypeToGenerate, nodesToHandle);
-                }
-            }
-
-            ClearMapNodeScratch(graphHead);
-            StripEmptyHallWays(graphHead);
-
-            return graphHead;
-        }
-
-        private void PrintMapGraph(MapNode current, int indent)
-        {
-            if (current.Scratch == 3)
-                return;
-
-            current.Scratch = 3;
-            string tabString = String.Empty;
-            for (int i = 0; i < indent; i++)
-            {
-                tabString += "\t";
-            }
-            System.Console.Out.WriteLine(tabString + current.Type.ToString() + " " + current.UniqueID.ToString());
-
-            foreach (MapNode n in current.Neighbors)
-                PrintMapGraph(n, indent + 1);
-        }
-
-        private List<MapNode> GenerateMapNodeList(MapNode headNode)
-        {
-            List<MapNode> nodeList = new List<MapNode>();
-            GenerateMapNodeListCore(headNode, nodeList);
-            return nodeList;
-        }
-
-        private void GenerateMapNodeListCore(MapNode currentNode, List<MapNode> nodeList)
-        {
-            if (nodeList.Contains(currentNode))
-                return;
-            nodeList.Add(currentNode);
-            foreach (MapNode n in currentNode.Neighbors)
-                GenerateMapNodeListCore(n, nodeList);
-        }
-
-        private void ClearMapNodeScratch(MapNode graphHead)
-        {
-            List<MapNode> nodeList = GenerateMapNodeList(graphHead);
-            foreach (MapNode n in nodeList)
-            {
-                n.Scratch = 0;
-            }
-        }
-
-        private void StripEmptyHallWays(MapNode graphHead)
-        {
-            bool anyRemoved;
-            do
-            {
-                anyRemoved = false;
-
-                List<MapNode> nodeList = GenerateMapNodeList(graphHead);
-                foreach (MapNode n in nodeList)
-                {
-                    if (n.Type == MapNodeType.Hall)
-                    {
-                        MapNode neightborOne = n.Neighbors[0];
-                        MapNode neightborTwo = n.Neighbors[1];
-                        if (neightborOne.Type == MapNodeType.None)
-                        {
-                            neightborTwo.RemoveNeighbor(n);
-                            neightborTwo.AddNeighbor(new MapNode(MapNodeType.None));
-                            anyRemoved = true;
-                        }
-                        else if (neightborTwo.Type == MapNodeType.None)
-                        {
-                            neightborOne.RemoveNeighbor(n);
-                            neightborOne.AddNeighbor(new MapNode(MapNodeType.None));
-                            anyRemoved = true;
-                        }
-                    }
-                }
-            }
-            while (anyRemoved);
-        }
-
-        private static void AddNeighborsToNode(MapNode parent, MapNodeType type, Queue<MapNode> nodeQueue)
-        {
-            MapNode neightbor = new MapNode(type);
-            parent.AddNeighbor(neightbor);
-            neightbor.AddNeighbor(parent);
-            nodeQueue.Enqueue(neightbor);
         }
 
         private void LoadChunksFromFile(string fileName)
