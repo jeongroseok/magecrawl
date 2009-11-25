5d6df7ae17eb8d8a8fb4030f0af7f6ea4523106a
chris hamons <chris.hamons@gmail.com>
2009-11-24 19:15:48 -0600

    Add "Debugging Mode", where we don't see welcome dialog, no timeouts on dialogs, and no save on window close. Makes life debugging better. Triggered by debug build.

---
+++ Trunk/Utilities/Preferences.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Magecrawl.Utilities
+{
+    // At some point in the future, this should read from a file and contain user-visible preferences
+    public class Preferences
+    {
+        private static Preferences m_instance = new Preferences();
+        public static Preferences Instance
+        {
+            get
+            {
+                return m_instance;
+            }
+        }
+
+        // Should things like FPS meter, opening story, and stuff be shown
+        public bool DebuggingMode
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+
+    }
+}
