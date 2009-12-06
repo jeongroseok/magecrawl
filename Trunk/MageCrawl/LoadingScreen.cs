503414fd7d3f0cf3177632f28edebf6cf2260d87
chris hamons <chris.hamons@gmail.com>
2009-12-06 17:00:00 -0600

    Add loading screen that flushes to screen to prevent vista from claiming we're not responding.

---
+++ Trunk/MageCrawl/LoadingScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using libtcodWrapper;
+using Magecrawl.GameUI;
+
+namespace Magecrawl
+{
+    internal class LoadingScreen : System.IDisposable
+    {
+        private Timer m_timer;
+        private RootConsole m_console;
+
+        internal LoadingScreen(RootConsole console, string text)
+        {
+            console.DrawFrame(0, 0, UIHelper.ScreenWidth, UIHelper.ScreenHeight, true);
+            console.PrintLineRect(text, UIHelper.ScreenWidth / 2, UIHelper.ScreenHeight / 2, UIHelper.ScreenWidth, UIHelper.ScreenHeight, LineAlignment.Center);
+            console.Flush();
+
+            m_console = console;
+            m_timer = new Timer(OnTick, null, 0, 2000);
+        }
+
+        public void Dispose()
+        {
+            if(m_timer != null)
+                m_timer.Dispose();
+            m_timer = null;
+        }
+
+        private void OnTick(System.Object o)
+        {
+            m_console.Flush();
+        }
+    }
+}
