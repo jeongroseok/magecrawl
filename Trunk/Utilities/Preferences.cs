49d840fb861675f8da492d9ee97230701ef9b5cf
chris hamons <chris.hamons@gmail.com>
2009-12-30 09:03:16 -0600

    Intial stab at preferences read from file.

--- Trunk/Utilities/Preferences.cs
+++ Trunk/Utilities/Preferences.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Magecrawl.Utilities
 {
     // At some point in the future, this should read from a file and contain user-visible preferences
     public class Preferences
     {
+        private Dictionary<string, object> m_preferences;
         private static Preferences m_instance = new Preferences();
+
+        internal Preferences()
+        {
+            m_preferences = new Dictionary<string, object>();
+            
+            // First load default settings for everything
+            SetupDefaultSettings();
+
+            try
+            {
+                // Try to load the settings from a file
+                LoadSettings();
+            }
+            catch (Exception)
+            {
+                // And ignore if the preference files doesn't exist
+            }
+        }
+
         public static Preferences Instance
         {
             get
@@ -19,11 +42,7 @@ namespace Magecrawl.Utilities
         {
             get
             {
-#if DEBUG
-                return true;
-#else
-                return false;
-#endif
+                return (bool)m_preferences["DebuggingMode"];
             }
         }
 
@@ -31,8 +50,63 @@ namespace Magecrawl.Utilities
         {
             get
             {
-                return false;
+                return (bool)m_preferences["SinglePressOperate"];
             }
         }
+
+        private void SetupDefaultSettings()
+        {
+#if DEBUG
+            m_preferences["DebuggingMode"] = true;
+#else
+            m_preferences["DebuggingMode"] = false;
+#endif
+            m_preferences["SinglePressOperate"] = false;
+        }
+
+        private void LoadSettings()
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreWhitespace = true;
+            settings.IgnoreComments = true;
+            XmlReader reader = XmlReader.Create(new StreamReader("Preferences.xml"), settings);
+            reader.Read();  // XML declaration
+            reader.Read();  // KeyMappings element
+            if (reader.LocalName != "Preferences")
+            {
+                // We didn't find our preference file...
+                throw new System.IO.FileNotFoundException();
+            }
+            while (true)
+            {
+                reader.Read();
+                if (reader.NodeType == XmlNodeType.EndElement)
+                {
+                    // If we're at an end element, if its the preference section, break, else skip it
+                    if (reader.LocalName == "Preferences")
+                        break;
+                    else
+                        continue;
+                }
+
+                switch (reader.LocalName)
+                {
+                    case "DebuggingMode":
+                        ReadBooleanData(reader, "DebuggingMode");
+                        break;
+                    case "SinglePressOperate":
+                        ReadBooleanData(reader, "SinglePressOperate");
+                        break;
+                }
+            }
+            reader.Close();
+        }
+
+        private void ReadBooleanData(XmlReader reader, string preferenceName)
+        {
+            reader.Read();
+            m_preferences[preferenceName] = Boolean.Parse(reader.Value);
+        }
+
     }
 }
