4eb4227e3e6c2d7f1b89781005854a529b45584f
Chris Hamons <chris.hamons@gmail.com>
2010-01-25 23:18:14 -0600

    Add Headpiece, Gloves, Boots as armor types, still do nothing. Add missing files.

---
+++ Trunk/GameEngine/Armor/ArmorBase.cs
@@ -0,0 +1,17 @@
+﻿using Magecrawl.GameEngine.Interfaces;
+using Magecrawl.GameEngine.Items;
+
+namespace Magecrawl.GameEngine.Armor
+{
+    internal abstract class ArmorBase : ItemWithOwner, IArmor
+    {
+        internal ArmorBase(string name, string itemDescription, string flavorText) : base(null, name, itemDescription, flavorText)
+        {
+        }
+
+        public override object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Trunk/GameEngine/Armor/Headpiece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Magecrawl.GameEngine.Interfaces;
+
+namespace Magecrawl.GameEngine.Armor
+{
+    internal class Headpiece : ArmorBase
+    {
+        public Headpiece(string name, string description, string flavorText) : base(name, description, flavorText)
+        {
+        }
+
+        public override List<ItemOptions> PlayerOptions
+        {
+            get
+            {
+                List<ItemOptions> optionList = new List<ItemOptions>();
+
+                if (CoreGameEngine.Instance.Player.Headpiece == this)
+                    optionList.Add(new ItemOptions("Unequip", true));
+                else
+                {
+                    optionList.Add(new ItemOptions("Equip", true));
+                    optionList.Add(new ItemOptions("Drop", true));
+                }
+
+                return optionList;
+            }
+        }
+    }
+}
