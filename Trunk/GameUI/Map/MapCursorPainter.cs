5e4288f1807ce0a4022ff8c78b1b2cc37ed87fc0
Chris Hamons <chris.hamons@gmail.com>
2010-02-22 08:57:41 -0600

    Only show tooltips on view mode.

--- Trunk/GameUI/Map/MapCursorPainter.cs
+++ Trunk/GameUI/Map/MapCursorPainter.cs
@@ -35,33 +35,36 @@ namespace Magecrawl.GameUI.Map
             {
                 screen.SetCharBackground(ScreenCenter.X + 1, ScreenCenter.Y + 1, TCODColorPresets.DarkYellow);
 
-                if (TCODSystem.ElapsedMilliseconds - m_lastCursorMovedTime > TimeUntilToolTipPopup)
+                if (ToolTipsEnabled)
                 {
-                    if (m_currentToolTips.Count > 0)
+                    if (TCODSystem.ElapsedMilliseconds - m_lastCursorMovedTime > TimeUntilToolTipPopup)
                     {
-                        const int MaxNumberOfLinesToShow = 3;
-                        int numberOfLinesToShow = System.Math.Min(m_currentToolTips.Count, MaxNumberOfLinesToShow);
-                        
-                        int longestLine = 0;
-                        for (int i = 0; i < numberOfLinesToShow; ++i)
-                            longestLine = System.Math.Max(longestLine, m_currentToolTips[i].Length);
-                        
-                        // If we're going to need to print "...more..." make sure we have the width
-                        if (m_currentToolTips.Count > MaxNumberOfLinesToShow)
-                            longestLine = System.Math.Max(longestLine, 10);
-                        
-                        screen.BackgroundColor = TCODColorPresets.DarkGray;
+                        if (m_currentToolTips.Count > 0)
+                        {
+                            const int MaxNumberOfLinesToShow = 3;
+                            int numberOfLinesToShow = System.Math.Min(m_currentToolTips.Count, MaxNumberOfLinesToShow);
 
-                        int frameHeight = m_currentToolTips.Count > MaxNumberOfLinesToShow ? 3 + numberOfLinesToShow : 2 + numberOfLinesToShow;
-                        screen.DrawFrame(ScreenCenter.X + 2, ScreenCenter.Y - 2, longestLine + 2, frameHeight, false, new Background(BackgroundFlag.Multiply));
+                            int longestLine = 0;
+                            for (int i = 0; i < numberOfLinesToShow; ++i)
+                                longestLine = System.Math.Max(longestLine, m_currentToolTips[i].Length);
 
-                        for (int i = 0; i < numberOfLinesToShow; ++i)
-                            screen.PrintLine(m_currentToolTips[i], ScreenCenter.X + 3, ScreenCenter.Y - 1 + i, new Background(BackgroundFlag.Multiply), LineAlignment.Left);
-                        
-                        if (m_currentToolTips.Count > MaxNumberOfLinesToShow)
-                            screen.PrintLine("...more...", ScreenCenter.X + 3, ScreenCenter.Y - 1 + MaxNumberOfLinesToShow, new Background(BackgroundFlag.Multiply), LineAlignment.Left);
+                            // If we're going to need to print "...more..." make sure we have the width
+                            if (m_currentToolTips.Count > MaxNumberOfLinesToShow)
+                                longestLine = System.Math.Max(longestLine, 10);
 
-                        screen.BackgroundColor = TCODColorPresets.Black;
+                            screen.BackgroundColor = TCODColorPresets.DarkGray;
+
+                            int frameHeight = m_currentToolTips.Count > MaxNumberOfLinesToShow ? 3 + numberOfLinesToShow : 2 + numberOfLinesToShow;
+                            screen.DrawFrame(ScreenCenter.X + 2, ScreenCenter.Y - 2, longestLine + 2, frameHeight, false, new Background(BackgroundFlag.Multiply));
+
+                            for (int i = 0; i < numberOfLinesToShow; ++i)
+                                screen.PrintLine(m_currentToolTips[i], ScreenCenter.X + 3, ScreenCenter.Y - 1 + i, new Background(BackgroundFlag.Multiply), LineAlignment.Left);
+
+                            if (m_currentToolTips.Count > MaxNumberOfLinesToShow)
+                                screen.PrintLine("...more...", ScreenCenter.X + 3, ScreenCenter.Y - 1 + MaxNumberOfLinesToShow, new Background(BackgroundFlag.Multiply), LineAlignment.Left);
+
+                            screen.BackgroundColor = TCODColorPresets.Black;
+                        }
                     }
                 }
             }
@@ -75,6 +78,7 @@ namespace Magecrawl.GameUI.Map
         internal override void DisableAllOverlays()
         {
             m_isSelectionCursor = false;
+            ToolTipsEnabled = false;
         }
 
         internal bool MapCursorEnabled
@@ -89,5 +93,7 @@ namespace Magecrawl.GameUI.Map
                 NewCursorPosition();
             }
         }
+
+        internal bool ToolTipsEnabled { get; set; }
     }
 }
