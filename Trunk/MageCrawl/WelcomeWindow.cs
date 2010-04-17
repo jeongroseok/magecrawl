b756d6b0514d41e35be2a9f394a20922a4c2d83a
chris.hamons <chris.hamons@localhost>
2010-04-17 17:30:08 -0500

    Three of the seven intro texts done.

--- Trunk/MageCrawl/WelcomeWindow.cs
+++ Trunk/MageCrawl/WelcomeWindow.cs
@@ -27,7 +27,7 @@ namespace Magecrawl
         }
 
         private const int ScrollAmount = 3;
-        private const int LengthOfEachElement = 250;
+        private const int LengthOfEachElement = 1250;
         
         private const int SpellListOffset = 42;
         private const int EntryOffset = 8;
@@ -43,6 +43,7 @@ namespace Magecrawl
         private TCODRandom m_random;
         private Dictionary<MagicTypes, string> m_flavorText;
         private Dictionary<MagicTypes, List<string>> m_spellLists;
+        private Dictionary<MagicTypes, string> m_mainText;
         private MagicTypes m_currentElementPointedTo;
         private Result m_windowResult;
         private List<string> m_fileNameList;
@@ -65,7 +66,8 @@ namespace Magecrawl
             m_random = new TCODRandom();
             m_windowResult = new Result();
 
-            m_currentElementPointedTo = (MagicTypes)m_random.getInt(0, 6);    // Pick a random element to start on.
+            //m_currentElementPointedTo = (MagicTypes)m_random.getInt(0, 6);    // Pick a random element to start on.
+            m_currentElementPointedTo = MagicTypes.Arcane;
 
             m_flavorText = new Dictionary<MagicTypes, string>();
             m_flavorText[MagicTypes.Fire] = "Passionate fire-based magic. Aggressive front loaded damage. Weaker in magic requiring more finesse.";
@@ -75,6 +77,15 @@ namespace Magecrawl
             m_flavorText[MagicTypes.Light] = "Gifted from the Creator. Light magic is best at mending wounds, summoning aid and protection. Strong at smiting unholy abominations.";
             m_flavorText[MagicTypes.Darkness] = "Darkness magic is drawn from a pure lust for power. A mockery of the light, capable of producing much pain, summoning forbidden aid, and raising the dead to serve.";
             m_flavorText[MagicTypes.Arcane] = "Arcane magic is born from altering the fabric of reality. Infinitely flexible and neutral to all other school of magic.";
+
+            m_mainText = new Dictionary<MagicTypes, string>();
+            m_mainText[MagicTypes.Fire] = "It is our passions that seperate us from the other schools, our passion for life, our passion for knowledges, our passion for power.\n\nIt is your fiery passion that brought you to this school. Your instructors will shape your unrefined passion into power. Power to shape even the very flames beneath this mountain. \n\nTo be a pyromancer is to be channel forces so great as to be nearly out of control and yet hold onto it with your very will.\n\nRemember this as you enter these halls, as many have before you.\n\n\n\n\n\n\nSenior Pyromancer Nathan\nWelcoming Ceremony\n372 CT";
+            m_mainText[MagicTypes.Water] = "";
+            m_mainText[MagicTypes.Air] = "";
+            m_mainText[MagicTypes.Earth] = "";
+            m_mainText[MagicTypes.Light] = "";
+            m_mainText[MagicTypes.Darkness] = "Jonathan,\n\n     You were right, those hidden manuscripts we found were truthful. While I have not found the tomb yet, this swamp is full of power. The dead walk, wondering (patrolling?) near this bog, the very air is filled with a dread power stronger than anything at the academy.\n\nOnce I find the tomb, the spellbooks of the Lich Algmon shall be ours. With them, the power to shape the world. Those fools who speak of restraint or treaties lack the will to act. That is one thing we do not lack.\n\nAs soon as you can slip away, locate me using the charm attached. We can continue the search.\n\n\n\n\nAngelina\nCT 381";
+            m_mainText[MagicTypes.Arcane] = "Magic in its purest form is a sentient being pressing their will against nature. More accurately, their will distorts the weave of creation. By understanding this weave, a student of the arcane can wield force alone as their weapon, as opposed to crude elemental magics. \n\nWith study, the amount of distortion one can force into the weave before it \"snaps\" back and ruins a spell increases. Also, feats such as teleportation become possible.\n\nThis textbook will provide an introduction to Arcane magic. The study of the arcane provides both an excellent foundation for future studies in other areas and a path to high magic in itself.\n\n\nModern Exploration of the Weave: An Introduction to Modern Arcane theory\nPage 3\nDavid C. Fellow";
 
             m_spellLists = new Dictionary<MagicTypes, List<string>>();
             m_spellLists[MagicTypes.Fire] = new List<string>() { "Firebolt", "Warmth", "Fireblast", "Fireball", "Wall Of Fire", "Firestorm" };
@@ -238,7 +249,9 @@ namespace Magecrawl
             const int GraphicX = 32;
             const int GraphicY = 8;
 
+            numberOfLines = m_console.printRect(GraphicX + 2, GraphicY + 2, GraphicTextWidth - 4, 32, m_mainText[m_currentElementPointedTo]);
             m_console.printFrame(GraphicX, GraphicY, GraphicTextWidth, 32, false);
+
         }
 
         private void DrawSpellList()
