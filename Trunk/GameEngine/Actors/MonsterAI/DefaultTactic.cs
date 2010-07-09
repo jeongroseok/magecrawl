9b0c1ef6b59690c283614f74398e9cbbf37ba22d
chris hamons <chris.hamons@gmail.com>
2010-07-08 23:30:18 -0500

    Remove all monster sub classes and use AI profiles alone. Still need to rip list into xml. Actually cool down sling stone.

---
+++ Trunk/GameEngine/Actors/MonsterAI/DefaultTactic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Magecrawl.Utilities;
+
+namespace Magecrawl.GameEngine.Actors.MonsterAI
+{
+    class DefaultTactic : BaseTactic
+    {
+        public override bool CouldUseTactic(CoreGameEngine engine, Monster monster)
+        {
+            return true;
+        }
+
+        public override bool UseTactic(CoreGameEngine engine, Monster monster)
+        {
+            if (IsPlayerVisible(engine, monster))
+            {
+                List<Point> pathToPlayer = GetPathToPlayer(engine, monster);
+
+                if (IsNextToPlayer(engine, pathToPlayer))
+                {
+                    if (AttackPlayer(engine, monster))
+                        return true;
+                }
+
+                if (HasPathToPlayer(engine, pathToPlayer))
+                {
+                    if (MoveOnPath(engine, pathToPlayer, monster))
+                        return true;
+                }
+            }
+            else
+            {
+                if (WalkTowardsLastKnownPosition(engine, monster))
+                    return true;
+            }
+            WanderRandomly(engine, monster);   // We have nothing else to do, so wander                
+            return true;
+        }
+
+        public override bool NeedsPlayerLOS
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
