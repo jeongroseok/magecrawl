9a50d424c192c7e72a87de9d388a5d0d9eaecad4
chris hamons <chris.hamons@gmail.com>
2010-08-28 14:59:04 -0500

    Stop querying the game engine every frame for character information (which gets recalculated every time), cache it.

--- Trunk/GameUI/CharacterInfo.cs
+++ Trunk/GameUI/CharacterInfo.cs
@@ -11,7 +11,6 @@ namespace Magecrawl.GameUI
     {
         public CharacterInfo()
         {
-            m_player = null;
             m_currentLevel = -1;
             m_colorHelper = new DialogColorHelper();
             MapCursorEnabled = false;
@@ -26,7 +25,6 @@ namespace Magecrawl.GameUI
 
         private const int BarLength = 23;
 
-        private IPlayer m_player;
         private int m_currentLevel;
         private List<ICharacter> m_monstersNearby;
         private int m_turnCount;
@@ -36,11 +34,44 @@ namespace Magecrawl.GameUI
         internal bool MapCursorEnabled { get; set; }
         internal Point CursorSpot { get; set; }
 
+        private int m_currentHealth;
+        private int m_maxHealth;
+        private int m_currentStamina;
+        private int m_maxStamina;
+        private int m_currentHP;
+        private int m_maxHP;
+        private int m_currentMP;
+        private int m_maxMP;
+        private int m_maxPossibleMP;
+        private Point m_position;
+        private string m_name;
+        private IWeapon m_currentWeapon;
+        private List<IStatusEffect> m_statusEffects;
+        private int m_skillPoints;
+
         public override void UpdateFromNewData(IGameEngine engine, Point mapUpCorner, Point centerPosition)
         {
-            m_player = engine.Player;
+            IPlayer player = engine.Player;
+            m_name = player.Name;
+            m_position = player.Position;
+
+            m_currentHealth = player.CurrentHealth;
+            m_maxHealth = player.MaxHealth;
+            m_currentStamina = player.CurrentStamina;
+            m_maxStamina = player.MaxStamina;
+            m_currentHP = player.CurrentHP;
+            m_maxHP = player.MaxHP;
+
+            m_currentMP = player.CurrentMP;
+            m_maxMP = player.MaxMP;
+            m_maxPossibleMP = player.MaxPossibleMP;
+
+            m_currentWeapon = player.CurrentWeapon;
+            m_statusEffects = player.StatusEffects.ToList();
+            m_skillPoints = player.SkillPoints;
+
             m_currentLevel = engine.CurrentLevel;
-            m_monstersNearby = engine.GameState.MonstersInPlayerLOS().OrderBy(x => PointDirectionUtils.LatticeDistance(x.Position, m_player.Position)).ToList();
+            m_monstersNearby = engine.GameState.MonstersInPlayerLOS().OrderBy(x => PointDirectionUtils.LatticeDistance(x.Position, m_position)).ToList();
             m_turnCount = engine.TurnCount;
             m_inDanger = engine.GameState.CurrentOrRecentDanger();
         }
@@ -91,15 +122,15 @@ namespace Magecrawl.GameUI
                 return TCODColor.darkRed.Divide(2);
         }
 
-        private TCODColor PlayerHealthBarColorAtPosition(IPlayer player, int position)
+        private TCODColor PlayerHealthBarColorAtPosition(int position)
         {
-            int healthPortionOfPlayerHPBarLength = StandardBarLength(player.MaxHealth, player.MaxHP);
+            int healthPortionOfPlayerHPBarLength = StandardBarLength(m_maxHealth, m_maxHP);
             int staminaPortionOfPlayerHPBarLength = BarLength - healthPortionOfPlayerHPBarLength;
 
             if (position < healthPortionOfPlayerHPBarLength)
             {
-                double percentage = ((double)player.CurrentHealth / (double)player.MaxHealth) * 100;
-                if (position >= CalculateBarLength(player.CurrentHealth, player.MaxHealth, healthPortionOfPlayerHPBarLength))
+                double percentage = ((double)m_currentHealth / (double)m_maxHealth) * 100;
+                if (position >= CalculateBarLength(m_currentHealth, m_maxHealth, healthPortionOfPlayerHPBarLength))
                     return TCODColor.black;
                 if (percentage > 95)
                     return TCODColor.orange.Divide(2);
@@ -112,8 +143,8 @@ namespace Magecrawl.GameUI
             }
             else
             {
-                double percentage = ((double)player.CurrentStamina / (double)player.MaxStamina) * 100;
-                if (position - healthPortionOfPlayerHPBarLength >= CalculateBarLength(player.CurrentStamina, player.MaxStamina, staminaPortionOfPlayerHPBarLength))
+                double percentage = ((double)m_currentStamina / (double)m_maxStamina) * 100;
+                if (position - healthPortionOfPlayerHPBarLength >= CalculateBarLength(m_currentStamina, m_maxStamina, staminaPortionOfPlayerHPBarLength))
                     return TCODColor.black;
                 if (percentage > 95)
                     return TCODColor.red.Divide(2);
@@ -126,14 +157,14 @@ namespace Magecrawl.GameUI
             }
         }
 
-        private TCODColor PlayerManaBarColor(IPlayer player, int position)
+        private TCODColor PlayerManaBarColor(int position)
         {
-            int enabledPortionOfMPBarLength = StandardBarLength(player.MaxMP, player.MaxPossibleMP);
+            int enabledPortionOfMPBarLength = StandardBarLength(m_maxMP, m_maxPossibleMP);
 
             if (position < enabledPortionOfMPBarLength)
             {
-                double percentage = ((double)player.CurrentMP / (double)player.MaxMP) * 100;
-                if (position >= CalculateBarLength(player.CurrentMP, player.MaxMP, enabledPortionOfMPBarLength))
+                double percentage = ((double)m_currentMP / (double)m_maxMP) * 100;
+                if (position >= CalculateBarLength(m_currentMP, m_maxMP, enabledPortionOfMPBarLength))
                     return TCODColor.black;
                 if (percentage > 95)
                     return TCODColor.blue.Divide(1.5);
@@ -153,37 +184,37 @@ namespace Magecrawl.GameUI
         public override void DrawNewFrame(TCODConsole screen)
         {
             screen.printFrame(StartingX, 0, InfoWidth, InfoHeight, true);
-            screen.printEx(SectionCenter, 1, TCODBackgroundFlag.Set, TCODAlignment.CenterAlignment, m_player.Name);
+            screen.printEx(SectionCenter, 1, TCODBackgroundFlag.Set, TCODAlignment.CenterAlignment, m_name);
 
             // The 7 and 18 here are psydo-magical, since they place the text overlap just so it fits and doesn't go over for sane values
-            string healthString = string.Format("Hlth {0}/{1}", m_player.CurrentHealth, m_player.MaxHealth);
-            string staminaString = string.Format("Sta {0}/{1}", m_player.CurrentStamina, m_player.MaxStamina);
+            string healthString = string.Format("Hlth {0}/{1}", m_currentHealth, m_maxHealth);
+            string staminaString = string.Format("Sta {0}/{1}", m_currentStamina, m_maxStamina);
             screen.printEx(StartingX + 7, 2, TCODBackgroundFlag.None, TCODAlignment.CenterAlignment, healthString);
             screen.printEx(StartingX + 18, 2, TCODBackgroundFlag.None, TCODAlignment.CenterAlignment, staminaString);
             for (int j = 0; j < BarLength; ++j)
-                screen.setCharBackground(StartingX + 2 + j, 2, PlayerHealthBarColorAtPosition(m_player, j));
+                screen.setCharBackground(StartingX + 2 + j, 2, PlayerHealthBarColorAtPosition(j));
 
-            string manaString = string.Format("Mana {0}/{1}", m_player.CurrentMP, m_player.MaxMP);
+            string manaString = string.Format("Mana {0}/{1}", m_currentMP, m_maxMP);
             screen.printEx(StartingX + 13, 3, TCODBackgroundFlag.Set, TCODAlignment.CenterAlignment, manaString);
             for (int j = 0; j < BarLength; ++j)
-                screen.setCharBackground(StartingX + 2 + j, 3, PlayerManaBarColor(m_player, j));
+                screen.setCharBackground(StartingX + 2 + j, 3, PlayerManaBarColor(j));
 
             int nextAvailablePosition = 6;
 
-            string skillPointString = "Skill Points: " + m_player.SkillPoints;
+            string skillPointString = "Skill Points: " + m_skillPoints;
             screen.print(StartingX + 2, nextAvailablePosition, skillPointString);
             nextAvailablePosition += 2;
 
-            string needsLoadedString =  !m_player.CurrentWeapon.IsRanged || m_player.CurrentWeapon.IsLoaded ? "" : "(empty)";
-            int linesTaken = screen.printRect(StartingX + 2, nextAvailablePosition, UIHelper.ScreenWidth - StartingX - 3, 5, string.Format("Weapon: {0} {1}", m_player.CurrentWeapon.DisplayName, needsLoadedString));
+            string needsLoadedString = !m_currentWeapon.IsRanged || m_currentWeapon.IsLoaded ? "" : "(empty)";
+            int linesTaken = screen.printRect(StartingX + 2, nextAvailablePosition, UIHelper.ScreenWidth - StartingX - 3, 5, string.Format("Weapon: {0} {1}", m_currentWeapon.DisplayName, needsLoadedString));
             nextAvailablePosition += linesTaken + 1;
 
             m_colorHelper.SaveColors(screen);
-            if (m_player.StatusEffects.Count() > 0)
+            if (m_statusEffects.Count() > 0)
             {
                 screen.print(StartingX + 2, nextAvailablePosition, "Status Effects:");
                 int currentX = StartingX + 2 + 1 + 15;
-                foreach (IStatusEffect s in m_player.StatusEffects)
+                foreach (IStatusEffect s in m_statusEffects)
                 {
                     if (currentX + s.Name.Length >= UIHelper.ScreenWidth)
                     {
@@ -234,7 +265,7 @@ namespace Magecrawl.GameUI
                 string level = (m_currentLevel + 1).ToString();
                 screen.print(54, 41, "Level - " + level);
 
-                string position = m_player.Position.ToString();
+                string position = m_position.ToString();
                 screen.print(54, 42, position);
 
                 string fps = TCODSystem.getFps().ToString();
